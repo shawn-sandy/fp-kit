cbce40d54b7c4263a8ae47587f4f96cbb6c21c10
shawn <shawnsandy04@gmail.com>
2022-07-08 09:33:40 -0400

    build: 👷 package config for npm builds

---
+++ libs/react/fp-button/vite.package.config.ts
@@ -0,0 +1,33 @@
+/** @format */
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+      plugins: [react(), dts()],
+      esbuild: {
+        logOverride: { 'this-is-undefined-in-esm': 'silent' }
+      },
+      build: {
+        outDir: resolve(__dirname, '../lib'),
+        sourcemap: true,
+        lib: {
+          entry: resolve(__dirname, 'src/components/index.ts'),
+          name: 'Link',
+          formats: ['es', 'umd'],
+          fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom', 'styled-components'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+  },
+})
