cce442c8fed0b7d45721a4ae6704e64f2b8b50d2
shawn <shawnsandy04@gmail.com>
2022-07-15 09:20:54 -0400

    feat: ✨ add story for button component

---
+++ libs/react/fp-button/src/components/button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './button'
+
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+export default {
+  title: 'Elements/Buttons',
+  component: Button,
+  parameters: {
+    // status: {
+    //   type: 'beta',
+    // },
+  }
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => (<Button { ...args } />)
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Default Button',
+  onClick: () => console.log('Clicked Default Button')
+}
