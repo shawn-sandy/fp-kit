f17a4adfd238827d467046179a8c7336478ea003
shawn <shawnsandy04@gmail.com>
2022-07-17 17:46:41 -0400

    fix: 🔥 config import

--- libs/react/fp-button/vitest.config.ts
+++ libs/react/fp-button/vitest.config.ts
@@ -2,10 +2,9 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom", // add jsdom
