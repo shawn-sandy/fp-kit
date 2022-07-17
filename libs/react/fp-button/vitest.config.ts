45520da08d8633f7096b4a5553d653041d3abdb2
shawn <shawnsandy04@gmail.com>
2022-07-17 17:42:26 -0400

    perf: ⚡️ add performance checks

--- libs/react/fp-button/vitest.config.ts
+++ libs/react/fp-button/vitest.config.ts
@@ -1,18 +1,19 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite'
+
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom', // add jsdom
-    setupFiles: './src/test/setup.ts', // setup includes
-    coverage: { // config coverage
-      reporter: ['text', 'html'], // generate coverage reports
-      exclude: [
-        'node_modules/',
-        'src/test/setup.ts'
-      ],
-    },
-  },
-});
+    environment: "jsdom", // add jsdom
+    setupFiles: "./src/test/setup.ts", // setup includes
+    coverage: {
+      // config coverage
+      reporter: ["text", "html"], // generate coverage reports
+      exclude: ["node_modules/", "src/test/setup.ts"]
+    }
+  }
+})
