64b4ae4b08e883b7f5143a7d633dc7c71964154e
shawn <shawnsandy04@gmail.com>
2022-09-02 09:23:06 -0400

    fix: 🚑️ remove live-examples from storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
     // "@storybook/addon-interactions",
-    'storybook-addon-live-examples',
+    // 'storybook-addon-live-examples',
     // "@storybook/addon-storysource",
     "@geometricpanda/storybook-addon-badges"
   ],
