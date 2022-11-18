b011d26512cf1ac352a75a37697536628ed92620
shawn <shawnsandy04@gmail.com>
2022-11-18 17:47:28 -0500

    feat: 🌟 add image onError
    - add image story w/interaction test
    - add imageError story w/interaction test
    - image onError method

--- libs/react/fpkit/src/components/images/img.tsx
+++ libs/react/fpkit/src/components/images/img.tsx
@@ -1,37 +1,51 @@
-import FP from '../fp'
-import { ComponentProps } from '../../types'
-
-export interface ImgProps extends ComponentProps {
+export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   src: string
   alt: string
-  width?: string
-  height?: string
+  width: number
+  height?: number
   styles?: {}
   loading?: 'eager' | 'lazy'
+  renderStyles?: boolean
 }
 
-export const defaultStyles = {}
+type ImgProps = ImageProps
+
+export const defaultStyles = {
+  width: 'var(--img-w, 100%)',
+  height: 'var(--img-h, auto)',
+  objectFit: 'var(--img-obj-fit, cover)',
+  objectPosition: 'var(--img-position, center center)',
+  aspectRatio: 'var(--img-ratio, auto 2/3)',
+}
 
 const Img = ({
   src,
   alt,
-  width,
+  width = 480,
   height,
   styles,
   renderStyles = true,
   loading = 'lazy',
+  placeholder = 'https://via.placeholder.com/150?text=PLACEHOLDER',
   ...props
 }: ImgProps) => {
   const stylesObj = renderStyles ? defaultStyles : {}
+
+  const handleImgError = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>,
+  ): void => {
+    const img = e.currentTarget
+    img.src = placeholder
+  }
   return (
-    <FP
-      as="img"
+    <img
       src={src}
       alt={alt}
       width={width}
-      height={height}
+      height={height || width}
       loading={loading}
-      styles={{ ...defaultStyles }}
+      style={{ ...styles, ...stylesObj }}
+      onError={handleImgError}
       {...props}
     />
   )
