4794eb2d68608aeecebaf8f1007f762517ad96f0
shawn <shawnsandy04@gmail.com>
2022-11-19 09:11:15 -0500

    build: ⚙️ update to use names imports

--- libs/react/fpkit/src/index.ts
+++ libs/react/fpkit/src/index.ts
@@ -1,6 +1,6 @@
 import {Badge} from './components/badge/fp-badge'
-import Button from './components/buttons/button'
-import Card from './components/cards/card'
+import {Button} from './components/buttons/button'
+import {Card} from './components/cards/card'
 import Dropdown from './components/dropdowns/dropdown'
 
 export { default as FP } from './components/fp'
