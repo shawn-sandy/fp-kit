84730971e9646fd24fc6af669242890a374c7ded
shawn <shawnsandy04@gmail.com>
2022-11-23 16:17:02 -0500

    feat: 🚧 stub out the modal component function

--- libs/react/fpkit/src/components/modal/dialog.tsx
+++ libs/react/fpkit/src/components/modal/dialog.tsx
@@ -1,16 +1,40 @@
-import FP from "../fp"
-import { ComponentProps } from "../../types"
+import FP from '../fp'
+import { ComponentProps } from '../../types'
+import { Button } from '../buttons/button'
 
 export interface DialogProps extends ComponentProps {
-  id: String
   open: Boolean
   header: React.ReactNode
   footer: React.ReactNode
   modalRef: React.RefObject<HTMLDialogElement>
 }
 
-const Dialog = ({children}: DialogProps) => {
-  return <FP as="dialog">{children}</FP>
+export const Dialog = ({ id, children }: DialogProps) => {
+  return (
+    <FP as="dialog" id={id}>
+      {children}
+    </FP>
+  )
+}
+
+export interface childDialogProps extends ComponentProps {
+  modalRef: React.RefObject<HTMLDialogElement>
+}
+
+export const closeDialog = ({ modalRef, children }: childDialogProps) => {
+  return (
+    <Button type="button" onPointerDown={() => modalRef.current?.close()}>
+      {children}
+    </Button>
+  )
+}
+
+export const openDialog = ({ modalRef, children }: childDialogProps) => {
+  return (
+    <Button type="button" onPointerDown={() => modalRef.current?.showModal()}>
+      {children}
+    </Button>
+  )
 }
 
 export default Dialog
