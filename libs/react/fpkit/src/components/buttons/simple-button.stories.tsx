51a9773e8cc9a13682d5d176869b9bc51ae290dd
shawn <shawnsandy04@gmail.com>
2022-12-02 07:07:53 -0500

    feat: 🌟 add story using CSF 3.0

---
+++ libs/react/fpkit/src/components/buttons/simple-button.stories.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./button"
+
+export default {
+  title: "FP.React Components/Buttons",
+  component: Button,
+  args: {
+    children: "Default Button",
+    type: "button",
+  },
+  argTypes: {
+    children: { control: "text" },
+    onPointerDown: { action: "down" },
+    onPointerLeave: { action: "leave" },
+    onPointerOver: { action: "over" }
+  }
+}
+
+export const Default = {}
