b45294e45a4260023e735de6a072b5c0794cb2a9
shawn <shawnsandy04@gmail.com>
2022-12-03 09:25:26 -0500

    feat: 🚧 update play feature to CSF3 story demo

--- libs/react/fpkit/src/components/buttons/simple-button.stories.tsx
+++ libs/react/fpkit/src/components/buttons/simple-button.stories.tsx
@@ -1,51 +1,44 @@
-import { Button } from "./button"
-import { BADGE } from "@geometricpanda/storybook-addon-badges"
+import { Button } from './button'
+import { BADGE } from '@geometricpanda/storybook-addon-badges'
+import '@shawnsandy/first-paint/dist/css/components/button.min.css'
 
-import {
-  userEvent,
-  screen,
-  waitFor
-} from "@storybook/testing-library"
-import { expect } from "@storybook/jest"
+import { userEvent, screen, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 
 export default {
-  title: "FP.React Components/Buttons",
+  title: 'FP.React Components/Buttons',
   component: Button,
   args: {
-    children: "Default Button",
-    type: "button",
+    children: 'Default Button',
+    type: 'button',
   },
   argTypes: {
-    children: { control: "text" },
-    onPointerDown: { action: "down" },
-    onPointerLeave: { action: "leave" },
-    onPointerOver: { action: "over" }
+    children: { control: 'text' },
+    onPointerDown: { action: 'down' },
+    onPointerLeave: { action: 'leave' },
+    onPointerOver: { action: 'over' },
   },
   parameters: {
     badges: [BADGE.BETA],
     docs: {
       description: {
-        component: "A headless button component"
-      }
-    }
-  }
-
+        component: 'A headless button component',
+      },
+    },
+  },
 }
 
 export const Default = {}
 export const Primary = {
   args: {
-    children: "Primary Button",
-    type: "submit",
+    children: 'Primary Button',
+    type: 'submit',
   },
   play: async () => {
     // const canvas = within(canvasElement)
-  const button = screen.getByRole("button")
-    userEvent.click(button)
-    await waitFor(() => {
-      expect(button).toBeInTheDocument()
-      expect(button).toHaveAccessibleName(/primary button/i)
-    })
-  }
-}
+    const button = screen.getByRole('button')
 
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveAccessibleName(/primary button/i)
+  },
+}
