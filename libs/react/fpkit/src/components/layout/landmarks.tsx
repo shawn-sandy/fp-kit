bf535aec0e5d4d12aae88fb7cd7b910efc624f62
shawn <shawnsandy04@gmail.com>
2023-01-22 17:47:14 -0500

    feat: 🚧 replace landmark w/ComponentProps

--- libs/react/fpkit/src/components/layout/landmarks.tsx
+++ libs/react/fpkit/src/components/layout/landmarks.tsx
@@ -2,57 +2,68 @@
  * @fileoverview Landmarks components
  */
 
-import FP from "../fp";
-import { ComponentProps } from "../../types";
+import FP from '../fp'
+import { ComponentProps } from '../../types'
 
-
-
-export interface LandmarkProps extends ComponentProps {}
-
-export const Header = ({ children, styles ={}, ...props }: ComponentProps) => {
+export const Header = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="header" {...props} styles={styles}>
       <FP as="section">{children}</FP>
     </FP>
-  );
-};
+  )
+}
 
 export const Main = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="main" styles={styles} {...props}>
       {children}
     </FP>
-  );
-};
+  )
+}
 
 export const Footer = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="footer" styles={styles} {...props}>
-      <FP as="section">{children || "Copyright © 2022"}</FP>
+      <FP as="section">{children || 'Copyright © 2022'}</FP>
     </FP>
-  );
-};
+  )
+}
 
 export const Aside = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="aside" styles={styles} {...props}>
       <FP as="section">{children}</FP>
     </FP>
-  );
-};
+  )
+}
 
-export const Section = ({ children, styles = {}, ...props }: ComponentProps) => {
-  return <FP as="section" styles={styles} {...props}>{children}</FP>;
-};
+export const Section = ({
+  children,
+  styles = {},
+  ...props
+}: ComponentProps) => {
+  return (
+    <FP as="section" styles={styles} {...props}>
+      {children}
+    </FP>
+  )
+}
 
+export const Article = ({
+  children,
+  styles = {},
+  ...props
+}: ComponentProps) => {
+  return (
+    <FP as="article" styles={styles} {...props}>
+      {children}
+    </FP>
+  )
+}
 
-export const Article = ({ children, styles = {}, ...props }: ComponentProps) => {
-  return <FP as="article" styles={styles} {...props}>{children}</FP>;
-};
-
-Header.displayName = "Header";
-Main.displayName = "Main";
-Footer.displayName = "Footer";
-Aside.displayName = "Aside";
-Section.displayName = "Section";
-Article.displayName = "Article";
+Header.displayName = 'Header'
+Main.displayName = 'Main'
+Footer.displayName = 'Footer'
+Aside.displayName = 'Aside'
+Section.displayName = 'Section'
+Article.displayName = 'Article'
