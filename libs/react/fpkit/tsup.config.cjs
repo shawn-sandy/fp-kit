1dc110c67581dbdb43fb6f8caa07da2fe5fea96d
shawn <shawnsandy04@gmail.com>
2023-03-26 23:00:15 -0400

    build: 📦 add external to tsup config

--- libs/react/fpkit/tsup.config.cjs
+++ libs/react/fpkit/tsup.config.cjs
@@ -4,7 +4,8 @@ export default defineConfig({
   entry: ['src/index.ts'],
   splitting: true,
   sourcemap: true,
+  external: ['react', 'react-dom'],
   clean: true,
   format: ['esm', 'cjs'],
-  minify: true
+  minify: true,
 })
