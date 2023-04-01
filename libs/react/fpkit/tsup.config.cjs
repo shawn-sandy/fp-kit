ff3009962aff5058c10fec9711c675bf6f507385
shawn <shawnsandy04@gmail.com>
2023-04-01 09:19:53 -0400

    build: 📦 update the tsup
    - add the `--external` option to the tsup config
    - add tree-shaking to the tsup config

--- libs/react/fpkit/tsup.config.cjs
+++ libs/react/fpkit/tsup.config.cjs
@@ -4,6 +4,8 @@ export default defineConfig({
   entry: ['src/index.ts'],
   splitting: true,
   sourcemap: true,
+  dts: true,
+  treeshake: true,
   external: ['react', 'react-dom'],
   clean: true,
   format: ['esm', 'cjs'],
