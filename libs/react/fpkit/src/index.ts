899cc0649f114b0048cab2617d8228c6c35ad55a
shawn <shawnsandy04@gmail.com>
2023-04-22 05:42:31 -0400

    feat: 🤩 first release link component

--- libs/react/fpkit/src/index.ts
+++ libs/react/fpkit/src/index.ts
@@ -1,4 +1,3 @@
-
 // export { Textarea } from './components/form/textarea';
 export { Badge } from './components/badge/fp-badge'
 export { Button } from './components/buttons/button'
@@ -14,8 +13,10 @@ export { Input } from './components/form/inputs'
 export { Field } from './components/form/fields'
 export { Textarea } from './components/form/textarea'
 export { Text, Title } from './components/text/text'
+export { Link } from './components/link/link'
 export { Header, Main, Footer, Article, Aside, Section } from './components/layout/landmarks'
 
+export { default as To } from './components/link/link'
 export { default as FP } from './components/fp'
 export { default as Kit } from './components/fp'
 export { default as Box } from './components/fp'
