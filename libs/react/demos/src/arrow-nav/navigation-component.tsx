09079a6ba1755c5aa83283dc52920fb32a6098d7
shawn <shawnsandy04@gmail.com>
2023-05-17 01:36:41 -0400

    feat(arrow-nav): add support for clicking on an item in the list and logging a message to the console
    feat(arrow-nav): add support for using arrow keys to navigate a list
    feat(arrow-nav): add support for using a ref to the section that contains the list to enable arrow key navigation

--- libs/react/demos/src/arrow-nav/navigation-component.tsx
+++ libs/react/demos/src/arrow-nav/navigation-component.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './MyComponent.css' // Import your css file
 import useArrowNavigation from './useArrowNavigation'
 
+/**
+ * Example arrow navigation component
+ * @returns
+ */
 const NavigationComponent: React.FC = () => {
-  const currentSelection = useArrowNavigation(7)
+  const sectionRef = useRef<HTMLUListElement>(null)
+  const currentSelection = useArrowNavigation(7, sectionRef)
 
   return (
-    <section>
-      <ul>
-        {Array.from({ length: 7 }, (_, index) => (
-          <li
-            key={index}
-            className={currentSelection === index ? 'selected' : ''}
-          >
-            {`Element ${index + 1}`}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul ref={sectionRef} tabIndex={0}>
+      {Array.from({ length: 7 }, (_, index) => (
+        <li
+          key={index}
+          className={currentSelection === index ? 'selected' : ''}
+          onClick={() => {
+            console.log('clicked')
+          }}
+        >
+          {`Element ${index + 1}`}
+        </li>
+      ))}
+    </ul>
   )
 }
 
--- libs/react/demos/src/arrow-nav/useArrowNavigation.tsx
+++ libs/react/demos/src/arrow-nav/useArrowNavigation.tsx
@@ -1,13 +1,41 @@
 import { useEffect, useState } from 'react'
 
-const useArrowNavigation = (itemCount: number) => {
+/**
+ * Use arrow keys to navigate a list
+ * @param itemCount
+ * @param sectionRef
+ * @returns
+ */
+const useArrowNavigation = (
+  /**
+   * The number of items in the list
+   */
+  itemCount: number,
+  /**
+   * The ref of the section that contains the list
+   * @example const sectionRef = useRef<HTMLUListElement>(null)
+   * @example const currentSelection = useArrowNavigation(7, sectionRef)
+   * @example <ul ref={sectionRef} tabIndex={0}>
+   */
+  sectionRef:
+    | React.RefObject<HTMLElement>
+    | React.RefObject<HTMLDivElement>
+    | React.RefObject<HTMLUListElement>,
+) => {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 'ArrowDown') {
+      if (
+        !sectionRef.current ||
+        sectionRef.current !== document.activeElement
+      ) {
+        return
+      }
+
+      if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
         setCurrentIndex((prevState) => (prevState + 1) % itemCount)
-      } else if (event.key === 'ArrowUp') {
+      } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
         setCurrentIndex((prevState) => (prevState - 1 + itemCount) % itemCount)
       }
     }
@@ -16,7 +44,7 @@ const useArrowNavigation = (itemCount: number) => {
     return () => {
       window.removeEventListener('keydown', handleKeyPress)
     }
-  }, [itemCount])
+  }, [itemCount, sectionRef])
 
   return currentIndex
 }
