37197eefc1e13aa062176a3c2cba3a4ab2264da9
shawn <shawnsandy04@gmail.com>
2023-07-22 09:00:53 -0400

    feat(button.test.tsx): add unit tests for Button component
    The new file `button.test.tsx` contains unit tests for the `Button` component. The tests cover the following scenarios:
    
    1. Renders a button element with the correct label.
    2. Calls the `onClick` handler when clicked.
    3. Applies custom styles when provided.
    
    These tests ensure that the `Button` component functions as expected and that it can be tested for different use cases.

---
+++ libs/react/fpkit/src/components/buttons/button.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import { Button } from './button'
+import jest from 'jest-mock'
+
+describe('Button', () => {
+  it('renders a button element with the correct label', () => {
+    const { getByText } = render(<Button type="button">Click me</Button>)
+    const button = getByText('Click me')
+    expect(button.tagName).toBe('BUTTON')
+  })
+
+  it('calls the onClick handler when clicked', () => {
+    const handleClick = jest.fn()
+    const { getByText } = render(
+      <Button type="button" onClick={handleClick}>
+        Click me
+      </Button>,
+    )
+    const button = getByText('Click me')
+    fireEvent.click(button)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('applies custom styles when provided', () => {
+    const { getByText } = render(
+      <Button type="button" styles={{ backgroundColor: 'red' }}>
+        Click me
+      </Button>,
+    )
+    const button = screen.getByRole('button')
+    screen.debug(button)
+    expect(button).toHaveAttribute('style', 'background-color: red;')
+  })
+})
