7da04ffc20edb59823e7052366954bb34037b9e4
shawn <shawnsandy04@gmail.com>
2023-07-22 13:32:43 -0400

    revert: 🚑️ restore original stories after update

---
+++ libs/react/fpkit/src/components/badge/index.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+// import '@shawnsandy/first-paint/dist/css/components/badge.min.css'
+import { BADGE } from "@geometricpanda/storybook-addon-badges"
+
+import {
+  ComponentStory,
+  ComponentMeta
+} from "@storybook/react"
+
+import {
+  within,
+  userEvent,
+  waitFor
+} from "@storybook/testing-library"
+import { expect } from "@storybook/jest"
+
+import {Badge} from "./fp-badge"
+export default {
+  title: "FP.React Components/Badge",
+  component: Badge,
+  argTypes: {
+    children: { control: "text" },
+    role: {
+      control: "select",
+      options: ["note", "alert", "status"]
+    }
+  },
+  parameters: {
+    badges: [BADGE.BETA]
+  }
+} as ComponentMeta<typeof Badge>
+
+const Template: ComponentStory<typeof Badge> = (args) => (
+  <Badge {...args}>{args.children}</Badge>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  children: "Badge",
+  role: "note"
+}
+
+Default.play = async ({ args, canvasElement }) => {
+  const { getByRole } = within(canvasElement)
+  const badge = getByRole("note")
+   expect(badge).toBeInTheDocument()
+   expect(badge).toHaveAttribute("style");
+}
---
+++ libs/react/fpkit/src/components/form/textarea.stories.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+// import '@shawnsandy/first-paint/dist/css/components/textarea.min.css'
+
+
+import {
+  ComponentStory,
+  ComponentMeta
+} from "@storybook/react"
+import { BADGE } from "@geometricpanda/storybook-addon-badges"
+
+import {
+  within,
+  userEvent,
+  waitFor
+} from "@storybook/testing-library"
+import { expect } from "@storybook/jest"
+
+import {Textarea} from "./textarea"
+export default {
+  title: "FP.React Components/Form/Inputs",
+  component: Textarea,
+  argTypes: {
+    children: { control: "text" },
+    // type: {
+    //   control: "select",
+    //   options: ["ComponentName", "submit", "reset"]
+    // },
+    // onPointerDown: { action: 'down' }
+    onChange: { action: 'change' },
+    onBlur: { action: 'blur' }
+  },
+  parameters: {
+    badges: [BADGE.BETA],
+    docs: {
+      description: {
+        component: "Component description",
+      },
+    }
+  }
+} as ComponentMeta<typeof Textarea>
+
+const Template: ComponentStory<typeof Textarea> = (args) => (
+  <Textarea {...args}/>
+)
+/**
+ *
+ */
+export const TextareaInput = Template.bind({})
+TextareaInput.args = {
+  rows: 8,
+}
+
+const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, voluptas? Nam natus fuga nostrum necessitatibus magnam vel deleniti autem! Expedita deserunt eum, quae officiis totam quo facere est minus consequatur"
+
+TextareaInput.play = async ({ args, canvasElement }) => {
+  const { getByRole } = within(canvasElement)
+  const ComponentName = getByRole("textbox")
+  expect(ComponentName).toBeInTheDocument()
+  expect(ComponentName).toHaveAttribute("rows")
+  userEvent.type(ComponentName, text, { delay: 10 })
+  expect(ComponentName).toHaveValue(text)
+}
