6d046fcfc53cca0d9da34958512f3c8ba0db9983
shawn <shawnsandy04@gmail.com>
2023-07-22 08:43:06 -0400

    feat(button.tsx): add comprehensive JSDoc comments to Button component to improve code documentation
    fix(button.tsx): fix import statement to use single quotes instead of double quotes for consistency
    feat(button.tsx): add defaultStyles object and defaultStyles prop to Button component to allow for customizable button styles
    feat(button.tsx): add handlePointerEvents function to handle pointer events and call corresponding event handlers
    fix(button.tsx): fix missing break statements in handlePointerEvents function to prevent fall-through behavior

--- libs/react/fpkit/src/components/buttons/button.tsx
+++ libs/react/fpkit/src/components/buttons/button.tsx
@@ -1,12 +1,11 @@
-import React from "react"
-
+import React from 'react'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
    * The type of the button.
    */
-  type: "button" | "submit" | "reset"
+  type: 'button' | 'submit' | 'reset'
 
   /**
    * Button label/content
@@ -44,18 +43,53 @@ export interface ButtonProps
   pointerLeave?: (e: React.PointerEvent) => void
 }
 
+/**
+ * Default styles object for the Button component.
+ *
+ * Defines CSS custom properties used for styling buttons.
+ *
+ * @property {string} --btn-px - Padding left/right
+ * @property {string} --btn-py - Padding top/bottom
+ * @property {string} --btn-dsp - Display value
+ * @property {string} --btn-place - place-items value
+ * @property {string} --btn-justify - justify-content value
+ * @property {string} --btn-cursor - Cursor value
+ * @property {string} --btn-border - Border value
+ * @property {string} --btn-color - Text color
+ * @property {string} --btn-bg - Background color
+ * @property {string} --btn-radius - Border radius
+ */
 export const defStyles = {
-  paddingInline: "var(--btn-px, 1.4rem)",
-  paddingBlock: "var(--btn-py, 0.8rem)",
-  display: "var(--btn-dsp, inline-flex)",
-  placeItems: "var(--btn-place, center)",
-  justifyContent: "var(--btn-justify, center)",
-  cursor: "var(--btn-cursor, pointer)",
-  border: "var(--btn-border, none)",
-  color: "var(--btn-color, white)",
-  backgroundColor: "var(--btn-bg, royalblue)",
-  borderRadius: "var(--btn-radius, 0.2rem)"
+  paddingInline: 'var(--btn-px, 1.4rem)',
+  paddingBlock: 'var(--btn-py, 0.8rem)',
+  display: 'var(--btn-dsp, inline-flex)',
+  placeItems: 'var(--btn-place, center)',
+  justifyContent: 'var(--btn-justify, center)',
+  cursor: 'var(--btn-cursor, pointer)',
+  border: 'var(--btn-border, none)',
+  color: 'var(--btn-color, white)',
+  backgroundColor: 'var(--btn-bg, royalblue)',
+  borderRadius: 'var(--btn-radius, 0.2rem)',
 }
+
+/*
+ * Button component
+ *
+ * Renders a <button> element with custom props.
+ *
+ * @param {ButtonProps} props - The component props
+ * @param {"button" | "submit" | "reset"} [props.type="button"] - The button type
+ * @param {ReactNode} props.children - The button label/content
+ * @param {Object} [props.styles] - Custom CSS styles
+ * @param {boolean} [props.disabled=false] - Whether the button is disabled
+ * @param {string} [props.classes] - Custom CSS classes
+ * @param {function} [props.pointerDown] - Pointer down event handler
+ * @param {function} [props.pointerOver] - Pointer over event handler
+ * @param {function} [props.pointerLeave] - Pointer leave event handler
+ * @param {boolean} [props.defaultStyles=false] - Whether to apply default styles
+ *
+ * @returns {JSX.Element} The rendered <button> element
+ */
 
 export const Button = ({
   type,
@@ -69,27 +103,23 @@ export const Button = ({
   defaultStyles = false,
   ...props
 }: ButtonProps) => {
-
   const defStyles = {}
 
   const stylesObj = defaultStyles ? defStyles : {}
-  const handlePointerEvents = (
-    e: React.PointerEvent<HTMLButtonElement>
-  ) => {
-    let eventType:String = e.type
+  const handlePointerEvents = (e: React.PointerEvent<HTMLButtonElement>) => {
+    let eventType: String = e.type
     if (!disabled) {
       switch (e.type) {
         case 'pointerover':
           pointerOver?.(e)
-          break;
+          break
         case 'pointerleave':
           pointerLeave?.(e)
-          break;
+          break
         case 'pointerdown':
-          if (e.button === 0)
-          pointerDown?.(e)
+          if (e.button === 0) pointerDown?.(e)
         default:
-          break;
+          break
       }
     }
   }
