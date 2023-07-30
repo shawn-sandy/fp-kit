26b6e5b3f00a4be280d3f65706e58516b0828246
shawn <shawnsandy04@gmail.com>
2023-07-29 20:54:14 -0400

    test(card.test.tsx): update import statement to include screen from @testing-library/react to fix test failure
    test(card.test.tsx): update getByText to use screen.getByText to fix test failure
    test(card.test.tsx): update getByTestId to use screen.getByTestId to fix test failure

--- libs/react/fpkit/src/components/cards/card.test.tsx
+++ libs/react/fpkit/src/components/cards/card.test.tsx
@@ -1,40 +1,40 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Card } from './card'
 
 describe('Card', () => {
   it('renders children', () => {
-    const { getByText } = render(<Card>Hello World</Card>)
-    expect(getByText('Hello World')).toBeInTheDocument()
+    render(<Card>Hello World</Card>)
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
   })
 
   it('renders with default styles', () => {
-    const { getByTestId } = render(
+    render(
       <Card data-testid="card" styles={Card.styles}>
         Hello World
       </Card>,
     )
-    const card = getByTestId('card')
+    const card = screen.getByTestId('card')
     expect(card).toHaveStyle(Card.styles)
   })
 
   it('renders with custom styles', () => {
     const style = { backgroundColor: '#000;' }
-    const { getByTestId } = render(
+    render(
       <Card data-testid="card" styles={style}>
         Hello World
       </Card>,
     )
-    const card = getByTestId('card')
+    const card = screen.getByTestId('card')
     expect(card).toHaveStyle(style)
   })
 
   it('renders with custom element', () => {
-    const { getByTestId } = render(
+    render(
       <Card data-testid="card" elm="section">
         Hello World
       </Card>,
     )
-    const card = getByTestId('card')
+    const card = screen.getByTestId('card')
     expect(card.tagName.toLowerCase()).toBe('section')
   })
 })
