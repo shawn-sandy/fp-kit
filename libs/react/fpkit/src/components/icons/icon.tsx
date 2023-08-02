fe7853660ea389057896f421f74e91c40151cb26
shawn <shawnsandy04@gmail.com>
2023-08-02 03:51:52 -0400

    feat(icons): add Icon component to fpkit library
    - Added a new file `icon.tsx` to the `fpkit` library under `components/icons` directory.
    - The `Icon` component is a reusable component that renders an SVG icon.
    - It accepts props such as `fill`, `size`, `viewBox`, and `name`.
    - The `Icon` component uses the `FP` component from `@/components/fp` and the `Code` icon from `@/components/icons`.
    - The default styles for the `Icon` component are defined in `defaultStyles`.
    - The `Icon` component is exported as the default export and has a display name of 'Icon'.
    - The `Icon` component also exports the `Code` icon as a named export.
    - Added a new file `types.ts` to the `fpkit` library under `components/icons` directory.
    - The `IconProps` interface is defined in `types.ts` and extends the `ComponentProps` interface.
    - The `IconProps` interface includes additional props such as `fill`, `size`, `strokeColor`, `viewBox`, and `name`.
    - Updated `tsconfig.json` in the `fpkit` library to include path aliases for `@components/*`, `@utils/*`, and `@/*`.
    
    The `Icon` component is added to the `fpkit` library to provide a convenient way to render SVG icons with customizable props.

---
+++ libs/react/fpkit/src/components/icons/icon.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import FP from '@/components/fp'
+import { ComponentProps } from '@/types'
+import { Code } from '@/components/icons'
+
+export interface IconProps extends Partial<ComponentProps> {
+  fill?: string
+  size?: string
+  viewBox?: string
+  name: string
+}
+
+export const defaultStyles = {
+  display: 'flex-inline',
+  direction: 'column',
+  fill: 'currentColor',
+}
+
+export const Icon = ({
+  children,
+  styles = defaultStyles,
+  ...props
+}: IconProps) => {
+  return (
+    <FP as="span" styles={styles} data-style="icons" {...props}>
+      {children}
+    </FP>
+  )
+}
+
+export default Icon
+Icon.displayName = 'Icon'
+Icon.styles = defaultStyles
+Icon.Code = Code
---
+++ libs/react/fpkit/src/components/icons/types.ts
@@ -0,0 +1,9 @@
+import { ComponentProps } from '@/types'
+
+export interface IconProps extends Partial<ComponentProps> {
+  fill?: string
+  size?: string
+  strokeColor?: string
+  viewBox?: string
+  name?: string
+}
