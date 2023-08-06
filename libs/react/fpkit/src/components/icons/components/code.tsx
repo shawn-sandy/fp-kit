a1df773150feeef968d5db0d98b6c67adbd95d5c
shawn <shawnsandy04@gmail.com>
2023-08-06 07:29:24 -0400

    fix(icons): change default fill color to 'none' and add stroke color to improve icon rendering
    feat(icons): update code icon to use a rectangle shape instead of polylines for better visual representation

--- libs/react/fpkit/src/components/icons/components/code.tsx
+++ libs/react/fpkit/src/components/icons/components/code.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
+
 import { IconProps } from '../types'
 
 const defaultStyles = {
-  fill: 'currentColor',
+  fill: 'none',
+  stroke: 'currentColor',
 }
 
 /**
@@ -11,30 +13,24 @@ const defaultStyles = {
  * @param {string} strokeColor - The color of the icon's stroke.
  * @param {string} fill - The color of the icon's fill.
  * @param {string} size - The size of the icon.
- * @param {React.CSSProperties} renderStyles - Additional styles to apply to the icon.
  * @param {string} viewBox - The viewBox of the icon.
- * @param {string} name - The name of the icon.
  * @param {React.CSSProperties} styles - The default styles of the icon.
  * @param {IconProps} props - Additional props to apply to the icon.
  * @returns {JSX.Element} - A React component that renders an SVG icon.
  */
 export const Code = ({
   strokeColor = 'currentColor',
-  fill = 'currentColor',
+  fill = 'none',
   size = '24',
-  renderStyles,
-  viewBox,
-  name,
   styles = defaultStyles,
   ...props
-}: IconProps) => {
+}: Pick<IconProps, 'strokeColor' | 'fill' | 'styles' | 'size'> = {}) => {
   return (
     <svg
       height={size}
       width={size}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      style={styles}
       {...props}
     >
       <g
@@ -45,8 +41,16 @@ export const Code = ({
         strokeMiterlimit="10"
         strokeWidth="2"
       >
-        <polyline fill="none" points="8 6 2 12 8 18" stroke={strokeColor} />
-        <polyline fill="none" points="16 6 22 12 16 18" />
+        <rect
+          height="22"
+          width="18"
+          fill="none"
+          stroke={strokeColor}
+          x="3"
+          y="1"
+        />
+        <polyline fill="none" points="9 9 6 12 9 15" />
+        <polyline fill="none" points="15 15 18 12 15 9" />
       </g>
     </svg>
   )
