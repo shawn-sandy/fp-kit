56c17795087d750ba560add9c9fbee83c4ccd78f
shawn <shawnsandy04@gmail.com>
2023-09-15 14:57:54 -0400

    docs: add jsdoc comments to popover component enable sourcemaps in tsconfig
    Add JSDoc comments to the Popover component to document props and usage.
    
    The comments explain what props the component accepts, what it returns, and how the popover logic works.
    
    This improves understanding of how to use the Popover component.
    
    The tsconfig.json file was also updated to enable sourcemaps for debugging.

--- libs/react/fpkit/src/hooks/popover/popover.tsx
+++ libs/react/fpkit/src/hooks/popover/popover.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import usePopover from './use-popover'
 
+/**
+* Interface for props accepted by the Popover component
+*
+* @property {ReactNode} children - The content to show in the popover
+* @property {ReactNode} [content] - Optional alternative content for popover
+*/
 export type PopoverProps = {
   children: React.ReactNode;
   content?: React.ReactNode;
 }
 
+/**
+* Popover component to display popover content.
+*
+* @param {Object} props - The props for the component.
+* @returns {JSX.Element} - The JSX element for the Popover component.
+*
+* The component uses the usePopover hook to handle popover visibility and positioning.
+*
+* It renders a button that shows the popover on hover using pointer events.
+*
+* The popover content itself is rendered absolutely positioned when visible.
+*
+* Styles like background, border, padding etc are applied inline.
+*
+* Transforms and opacity animate the popover enter/exit.
+*/
 export const Popover: React.FC = () => {
   const hoverRef = React.useRef(null)
   const popOverRef = React.useRef(null)
