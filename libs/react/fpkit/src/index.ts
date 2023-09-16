242beb2517543ff9797cedcde58b2a02d9ca53f5
shawn <shawnsandy04@gmail.com>
2023-09-16 17:18:50 -0400

    feat(fpkit): add Popover component to the library
    The Popover component was added to the fpkit library. This component provides functionality for displaying a popover.

--- libs/react/fpkit/src/index.ts
+++ libs/react/fpkit/src/index.ts
@@ -1,3 +1,4 @@
+import { Popover } from './hooks/popover/popover';
 // export { Textarea } from './components/form/textarea';
 export { Badge } from './components/badge/fp-badge'
 export { Button } from './components/buttons/button'
@@ -12,6 +13,7 @@ export { Input } from './components/form/inputs'
 export { Link } from './components/link/link'
 export { List } from './components/list/list'
 export { Modal } from './components/modal/modal'
+export { Popover } from './components/popover/popover'
 export { RenderTable as TBL } from './components/tables/table'
 export { Table, Caption, Thead, Tbody, Td, Tr } from "./components/tables/table-elements"
 export { Text, Title } from './components/text/text'
