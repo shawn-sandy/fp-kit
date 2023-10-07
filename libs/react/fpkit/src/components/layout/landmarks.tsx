887e0f7a9ef13c5ce5e2d7ec908d73ed374cc0e3
shawn <shawnsandy04@gmail.com>
2023-10-07 09:16:23 -0400

    fix: add jsdoc comments and remove unused code
    Added JSDoc comments to document landmarks components in landmarks.tsx.
    
    Removed unused path and TsconfigPathsPlugin imports in .storybook/main.js.
    
    Removed unused page config in .storybook/preview.tsx.
    
    Removed unnecessary @ts-ignore comments and args in landmarks.stories.tsx.
    
    Removed unused lint-staged script in package.json.

--- libs/react/fpkit/src/components/layout/landmarks.tsx
+++ libs/react/fpkit/src/components/layout/landmarks.tsx
@@ -4,7 +4,7 @@
 
 import FP from '../fp'
 import { ComponentProps } from '../../types'
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 /**
  * Renders children elements without any wrapping component.
@@ -12,6 +12,15 @@ import React from 'react'
  */
 export const Landmarks = (children?: React.FC) => <>{children}</>
 
+/**
+ * Header component.
+ *
+ * Renders a header landmark with a section child.
+ *
+ * @param children - The content to render inside the header.
+ * @param styles - Optional styles object.
+ * @param props - Other props.
+ */
 export const Header = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="header" {...props} styles={styles}>
@@ -20,6 +29,15 @@ export const Header = ({ children, styles = {}, ...props }: ComponentProps) => {
   )
 }
 
+/**
+ * Main component.
+ *
+ * Renders a main landmark.
+ *
+ * @param children - The content to render inside the main element.
+ * @param styles - Optional styles object.
+ * @param props - Other props.
+ */
 export const Main = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="main" styles={styles} {...props}>
@@ -28,6 +46,13 @@ export const Main = ({ children, styles = {}, ...props }: ComponentProps) => {
   )
 }
 
+/**
+ * Footer component that renders a footer element with a section element inside.
+ * @param {ReactNode} children - Child elements to render inside the section element.
+ * @param styles - CSS styles to apply to the footer element.
+ * @param props - Additional props to pass to the footer element.
+ * @returns A React component that renders a footer element with a section element inside.
+ */
 export const Footer = ({ children, styles = {}, ...props }: ComponentProps) => {
   return (
     <FP as="footer" styles={styles} {...props}>
@@ -44,6 +69,13 @@ export const Aside = ({ children, styles = {}, ...props }: ComponentProps) => {
   )
 }
 
+/**
+ * Section component that renders a section element.
+ *
+ * @param children - Child elements to render inside the section.
+ * @param styles - CSS styles to apply to the section.
+ * @param props - Other props.
+ */
 export const Section = ({
   children,
   styles = {},
@@ -56,6 +88,13 @@ export const Section = ({
   )
 }
 
+/**
+ * Article component renders an HTML <article> element.
+ *
+ * @param children - Child elements to render inside the article.
+ * @param styles - CSS styles to apply to the article.
+ * @param props - Additional props to pass to the article element.
+ */
 export const Article = ({
   children,
   styles = {},
