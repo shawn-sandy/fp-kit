5d03533bdbe7336156bbd36c243239464d171b0e
shawn <shawnsandy04@gmail.com>
2023-11-04 02:53:22 -0400

    fix: correct text-align value in _footer.scss
    The text-align property value was incorrectly set to '--text-align' instead of 'text-align' in the _footer.scss module. This fixes that typo to properly align the text.

--- libs/react/fpkit/src/components/nav/nav-items.tsx
+++ libs/react/fpkit/src/components/nav/nav-items.tsx
@@ -1,6 +1,12 @@
 import FP from '#components/fp'
 import React from 'react'
 
+/**
+ * The NavItemsProps type is used in TypeScript React to define the props for a component that renders
+ * children.
+ * @property children - The `children` property is used to render the content inside the `NavItems`
+ * component. It accepts any valid React node as its value, such as text, components, or elements.
+ */
 export type NavItemsProps = {
   /** Children to render. */
   children: React.ReactNode
@@ -9,10 +15,14 @@ export type NavItemsProps = {
 const defaultStyles = {}
 
 /**
- * Renders a list of navigation items.
+ * NavItems component
  *
- * @param {NavItemsProps} props - The props object containing children and other props.
- * @returns {JSX.Element} - The rendered NavItems component.
+ * Renders a `NavItems` component with children.
+ *
+ * @param {NavItemsProps} props - Component props
+ * @param {ReactNode} props.children - Content inside the `NavItems` component
+ *
+ * @returns {ReactElement} The `NavItems` component
  */
 const NavItems = ({ children, ...props }: NavItemsProps): JSX.Element => {
   return (
