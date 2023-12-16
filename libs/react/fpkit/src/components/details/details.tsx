9e9cb9dc35ce2d36b4dd982888bd2450263fe6ae
shawn <shawnsandy04@gmail.com>
2023-12-15 20:08:46 -0500

    fix: increase details component height and add animations
    - Increase default height of closed details from 4.6875rem to 50rem when open
    - Add CSS transition for smooth open/close animation
    - Add callbacks for toggle open/close and pointer down events
    - Update docs and prop types

--- libs/react/fpkit/src/components/details/details.tsx
+++ libs/react/fpkit/src/components/details/details.tsx
@@ -1,17 +1,73 @@
 import React from 'react'
 import FP from '#components/fp'
 export type DetailsProps = {
+  /**
+   * The content to render inside the details element.
+   * Required.
+   */
   children: React.ReactNode
+
+  /**
+   * The summary text shown for the details.
+   * Required.
+   */
   summary: React.ReactNode
+
+  /**
+   * Optional styles to apply to the details element.
+   */
   styles?: React.CSSProperties | {}
+
+  /**
+   * Optional callback when the details is toggled open/closed.
+   */
+  onToggleEvent?: (e: React.PointerEvent<HTMLDetailsElement>) => void
+
+  /**
+   * Optional callback when pointer down event occurs on the summary.
+   */
+  onPointerDownEvent?: (e: React.PointerEvent<HTMLDetailsElement>) => voidd
 }
 
-const Details = ({ summary, styles, children, ...props }: DetailsProps) => {
+/**
+ * Details component props.
+ * @typedef {Object} DetailsProps
+ * @property {ReactNode} children - The content to render inside the details element. Required.
+ * @property {ReactNode} summary - The summary text shown for the details. Required.
+ * @property {Object|CSSProperties} [styles] - Optional styles to apply to the details element.
+ * @property {(e: PointerEvent<HTMLDetailsElement>) => void} [onToggleEvent] - Optional callback when the details is toggled open/closed.
+ * @property {(e: PointerEvent<HTMLDetailsElement>) => void} [onPointerDownEvent] - Optional callback when pointer down event occurs on the summary.
+ * @param {DetailsProps} props
+ * @returns {JSX.Element}
+ */
+const Details = ({
+  summary,
+  styles,
+  onToggleEvent,
+  onPointerDownEvent,
+  children,
+  ...props
+}: DetailsProps) => {
   const defaultStyles: React.CSSProperties = { ...styles }
 
+  const onToggleCallback = (e: React.PointerEvent<HTMLDetailsElement>) => {
+    if (onToggleEvent) onToggleEvent?.(e)
+  }
+
+  const onPointerDownCallback = (e: React.PointerEvent<HTMLDetailsElement>) => {
+    if (onPointerDownEvent) onPointerDownEvent?.(e)
+  }
+
   return (
-    <FP as="details" {...props}>
-      <FP as="summary">{summary}</FP>
+    <FP
+      as="details"
+      style={defaultStyles}
+      onToggle={onToggleCallback}
+      {...props}
+    >
+      <FP as="summary" onPointerDown={onPointerDownCallback}>
+        {summary}
+      </FP>
       <FP as="section">{children}</FP>
     </FP>
   )
