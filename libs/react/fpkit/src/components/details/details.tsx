edc57ffa2fb3abe0541405b944afe9ee91c384cd
shawn <shawnsandy04@gmail.com>
2024-01-10 18:39:16 -0500

    Revert "fix(details): add aria-label for accessibility"
    This reverts commit cd0700841d797aaae4af33f3b96748ee02dad29f.

--- libs/react/fpkit/src/components/details/details.tsx
+++ libs/react/fpkit/src/components/details/details.tsx
@@ -48,7 +48,6 @@ export const Details = ({
   onToggleEvent,
   onPointerDownEvent,
   children,
-  ariaLabel,
   ref,
   ...props
 }: DetailsProps) => {
@@ -69,7 +68,6 @@ export const Details = ({
       onToggle={onToggleCallback}
       ref={ref}
       open={open}
-      aria-label={ariaLabel || 'Details dropdown'}
       {...props}
     >
       <FP as="summary" onPointerDown={onPointerDownCallback}>
