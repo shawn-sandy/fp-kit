837ea6bb6f4c837a5c2ec37ea065ca9d65a357a3
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2024-01-14 09:12:35 -0500

    Feat/text components (#431)
    Update TextProps, Span and Title types to extend new InheritedProps
    Add id prop to Text, Span and Title
    Rename TextProps elm prop to text
    Update Text and Title to accept classes prop
    Update Text, Span and Title to use id and classes props
    
    fix(text): refactor text component
    
    - refactor text component to use fp component
    - remove span component
    - update title component to use text
    - add sup and blockquote styles
    - update tests
    
    refactor(text): improve text components
    
    Refactored the text component to use the FP component. Removed the separate span component.
    Updated the title component to extend text instead of FP directly. Added some additional global styles for sup and blockquote.
    Updated tests.
    
    Co-authored-by: shawn <shawnsandy04@gmail.com>

--- libs/react/fpkit/src/components/text/text.tsx
+++ libs/react/fpkit/src/components/text/text.tsx
@@ -1,82 +1,85 @@
 import FP from '../fp'
 import React from 'react'
 
-export type TextElements =
-  | 'p'
-  | 'span'
+type InheritedProps = React.ComponentProps<typeof FP>
+
+type TextElements =
   | 'a'
-  | 'strong'
-  | 'em'
-  | 'small'
-  | 's'
+  | 'b'
+  | 'blockquote'
   | 'cite'
-  | 'time'
   | 'code'
+  | 'em'
+  | 'i'
   | 'kbd'
+  | 'mark'
+  | 'p'
+  | 's'
+  | 'small'
+  | 'span'
+  | 'span'
+  | 'strong'
   | 'sub'
   | 'sup'
-  | 'i'
-  | 'b'
+  | 'time'
   | 'u'
-  | 'mark'
-  | 'span'
-  | 'blockquote'
-export interface TextProps {
+
+export type TextProps = {
   /**
-   * HTML element
+   * Text element to to use
    */
   elm?: TextElements
-  children?: React.ReactNode
+  /** Pass a text element or string */
   text?: String
-  styles?: React.CSSProperties | {}
-}
+} & InheritedProps
 
+/**
+ * Text component - renders text based components.
+ * @param {Object} props - The props object.
+ * @param {string} [props.elm='p'] - The HTML element to render.
+ * @param {string} [props.id] - The id attribute.
+ * @param {string} [props.text] - The text content.
+ * @param {Object} [props.styles] - The styles object.
+ * @param {Object} [props.classes] - The classes object.
+ * @param {ReactNode} [props.children] - The children elements.
+ * @param {Object} [props] - Any other props.
+ * @returns {JSX.Element} - The Text component.
+ */
 export const Text = ({
   elm = 'p',
+  id,
   text,
   styles,
+  classes,
   children,
   ...props
 }: TextProps) => {
   return (
-    <FP as={elm} styles={styles} {...props}>
+    <FP as={elm} id={id} styles={styles} classNames={classes} {...props}>
       {children || text}
     </FP>
   )
 }
 
-export const Span = ({
-  elm = 'span',
-  children,
-  styles,
-  ...props
-}: TextProps) => {
-  return (
-    <FP as={elm} styles={styles} {...props}>
-      {children}
-    </FP>
-  )
-}
-
-export interface TitleProps {
+type TitleProps = {
   /**
    * HTML headings
    */
   elm?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-  children?: React.ReactNode
-  styles?: {}
-}
+} & InheritedProps
 
 export const Title = ({
   elm = 'h3',
+  id,
   children,
   styles,
+  classes,
   ...props
 }: TitleProps) => {
   return (
-    <FP as={elm} styles={styles} {...props}>
+    <Text as={elm} id={id} styles={styles} className={styles} {...props}>
       {children}
-    </FP>
+    </Text>
   )
 }
 
@@ -84,4 +87,3 @@ export default Text
 
 Text.displayName = 'Text'
 Title.displayName = 'Title'
-Span.displayName = 'Span'
