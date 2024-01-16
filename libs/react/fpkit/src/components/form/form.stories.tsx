1f7781e48a74bba074173edb52707716cac0d491
shawn <shawnsandy04@gmail.com>
2024-01-15 20:01:18 -0500

    fix: form component stories
    Update Form component stories to add sample usage in Storybook. Adds a play function to interact with the form in the canvas.
    
    Changes:
    
    Add sample Form component usage in stories
    Pass children prop with Form, Form.Field and Form.Input
    Add play function to fill and submit the form
    Add canvas interactions to submit and validate form
    This improves the Form stories by providing an example of how to use the component in a form with input fields. The play function allows interacting with the form in Storybook.

--- libs/react/fpkit/src/components/form/form.stories.tsx
+++ libs/react/fpkit/src/components/form/form.stories.tsx
@@ -3,6 +3,7 @@ import { within, userEvent, screen } from '@storybook/testing-library'
 import { expect } from '@storybook/jest'
 
 import Form from './form'
+import './form.scss'
 
 const meta: Meta<typeof Form> = {
   title: 'FP.REACT Forms/Components',
@@ -23,7 +24,24 @@ export default meta
 type Story = StoryObj<typeof Form>
 
 export const FormComponent: Story = {
-  args: {},
+  args: {
+    children: (
+      <>
+        <Form formAction={() => {}}>
+          <Form.Field label="Name" labelFor="name">
+            <Form.Input name="name" />
+          </Form.Field>
+          <Form.Field label="Email" labelFor="email">
+            <Form.Input name="email" type="email" />
+          </Form.Field>
+          <Form.Field label="Message" labelFor="message">
+            <Form.Textarea id="message" name="message" />
+          </Form.Field>
+          <button type="submit">Submit</button>
+        </Form>
+      </>
+    ),
+  },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
     expect(canvas.getByText(/link/i)).toBeInTheDocument()
