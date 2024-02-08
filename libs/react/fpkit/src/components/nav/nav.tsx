c0ac0243959b04c0bbe6e0994e00c270ac7665f3
shawn <shawnsandy04@gmail.com>
2024-02-07 21:58:31 -0500

    refactor: restructure nav components
    Restructured the nav components by removing the Navbar component and updating the Nav, NavList and NavItem components with JSDoc comments.

--- libs/react/fpkit/src/components/nav/nav.tsx
+++ libs/react/fpkit/src/components/nav/nav.tsx
@@ -8,6 +8,13 @@ export type NavItemProps = React.ComponentProps<typeof List.ListItem> & {
 }
 export type NavProps = React.ComponentProps<typeof UI>
 
+/**
+ * Renders a NavList component.
+ * @param {Object} props - The props for the component.
+ * @param {ReactNode} props.children - The child elements.
+ * @param {Object} props - Additional props to spread to the List component.
+ * @returns {JSX.Element} The rendered NavList component.
+ */
 export const NavList = ({ children, ...props }: NavListProps) => {
   return (
     <List type="ul" {...props} data-list="unstyled inline">
@@ -16,6 +23,17 @@ export const NavList = ({ children, ...props }: NavListProps) => {
   )
 }
 
+/**
+ * Renders a NavItem component.
+ * @param {Object} props - The props for the component.
+ * @param {string} [props.id] - The id for the component.
+ * @param {Object} [props.styles] - The styles for the component.
+ * @param {string} [props.classes] - The classes for the component.
+ * @param {ReactNode} props.children - The child elements.
+ * @param {boolean} [props.inline=true] - Whether the item should display inline.
+ * @param {Object} props - Additional props to spread to the ListItem component.
+ * @returns {JSX.Element} The rendered NavItem component.
+ */
 export const NavItem = ({
   id,
   styles,
@@ -38,6 +56,13 @@ export const NavItem = ({
   )
 }
 
+/**
+ * Renders a Nav component.
+ * @param {Object} props - The props for the component.
+ * @param {ReactNode} props.children - The child elements.
+ * @param {Object} props - Additional props to spread to the UI component.
+ * @returns {JSX.Element} The rendered Nav component.
+ */
 export const Nav = ({ children, ...props }: NavProps) => {
   return (
     <UI as="nav" {...props}>
