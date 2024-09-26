86e3639da66919eb5528c6556cf6efb1201f8c42
shawn <shawnsandy04@gmail.com>
2024-09-25 22:55:48 -0400

    feat(TextToSpeechControls): add component for text-to-speech controls

--- libs/react/fpkit/src/components/text-to-speech/views/TextToSpeechControls.tsx
+++ libs/react/fpkit/src/components/text-to-speech/views/TextToSpeechControls.tsx
@@ -26,38 +26,40 @@ const TextToSpeechControls: React.FC<TextToSpeechControlsProps> = ({
   return (
     <div data-tts>
       <p>{label || 'Read content'}</p>
-      <Button
-        aria-label="Speak"
-        type="button"
-        onClick={onSpeak}
-        disabled={isSpeaking}
-        data-btn="sm text"
-      >
-        <Icon.Play size={iconSize} />
-      </Button>
-      <Button
-        aria-label="Pause"
-        type="button"
-        onClick={onPause}
-        disabled={!isSpeaking || isPaused}
-        data-btn="sm text"
-      >
-        <Icon.Pause size={iconSize} />
-      </Button>
-      <Button
-        aria-label="Resume"
-        type="button"
-        onClick={onResume}
-        disabled={!isPaused}
-        data-btn="sm text"
-      >
-        <Icon.Resume size={iconSize} />
-      </Button>
+      {!isSpeaking && (
+        <Button
+          aria-label="Speak"
+          type="button"
+          onClick={onSpeak}
+          data-btn="sm text"
+        >
+          <Icon.Play size={iconSize} />
+        </Button>
+      )}
+      {isSpeaking && !isPaused && (
+        <Button
+          aria-label="Pause"
+          type="button"
+          onClick={onPause}
+          data-btn="sm text"
+        >
+          <Icon.Pause size={iconSize} />
+        </Button>
+      )}
+      {isPaused && (
+        <Button
+          aria-label="Resume"
+          type="button"
+          onClick={onResume}
+          data-btn="sm text"
+        >
+          <Icon.Resume size={iconSize} />
+        </Button>
+      )}
       <Button
         aria-label="Stop"
         type="button"
         onClick={onCancel}
-        disabled={!isSpeaking}
         data-btn="sm text"
       >
         <Icon.Stop size={iconSize} />
