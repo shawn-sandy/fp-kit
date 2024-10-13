ba9c632ff4765248c26d601c22c4541259d62317
shawn <shawnsandy04@gmail.com>
2024-10-13 01:26:20 -0400

    refactor(card): update card component structure and styling
    - modify card selector in scss to use data attributes
    - update card component to accept title and footer props
    - adjust card content structure in jsx
    - implement basic flexcard component
    - remove commented code and update component documentation

--- libs/react/fpkit/src/components/cards/card.tsx
+++ libs/react/fpkit/src/components/cards/card.tsx
@@ -10,46 +10,9 @@ import UI from '#components/ui'
  */
 export type CardProps = {
   elm?: 'div' | 'aside' | 'section' | 'article'
+  title?: React.ReactNode
+  footer?: React.ReactNode
 } & React.ComponentProps<typeof UI>
-
-/*
- * Card component
- *
- * Renders a card container.
- *
- * @param {CardProps} props - Component props
- * @param {('div' | 'aside' | 'section' | 'article')} [props.elm='div'] - Element to render as
- * @param {Object} [props.styles] - CSS styles to apply
- * @param {ReactNode} props.children - Card content
- * @param {boolean} [props.renderStyles=true] - Whether to render default styles
- * @param {string} [props.dataStyle] - data-card attribute value
- * @param {string} [props.id] - Unique ID
- *
- * @returns {ReactElement} Card component
- */
-export const Card = ({
-  elm = 'div',
-  styles,
-  children,
-  classes,
-  id,
-  ...props
-}: CardProps) => {
-  return (
-    <UI
-      as={elm}
-      id={id}
-      styles={styles}
-      className={classes}
-      data-card
-      {...props}
-    >
-      <UI as="div" data-card-content>
-        {children}
-      </UI>
-    </UI>
-  )
-}
 
 /*
  * Title component
@@ -122,6 +85,53 @@ export const Content = ({
 }
 
 Content.displayName = 'Content'
+
+/*
+ * Card component
+ *
+ * Renders a card container.
+ *
+ * @param {CardProps} props - Component props
+ * @param {('div' | 'aside' | 'section' | 'article')} [props.elm='div'] - Element to render as
+ * @param {Object} [props.styles] - CSS styles to apply
+ * @param {ReactNode} props.children - Card content
+ * @param {boolean} [props.renderStyles=true] - Whether to render default styles
+ * @param {string} [props.dataStyle] - data-card attribute value
+ * @param {string} [props.id] - Unique ID
+ *
+ * @returns {ReactElement} Card component
+ */
+export const Card = ({
+  elm = 'div',
+  styles,
+  children,
+  classes,
+  id,
+  footer,
+  title,
+  ...props
+}: CardProps) => {
+  return (
+    <UI
+      as={elm}
+      id={id}
+      styles={styles}
+      className={classes}
+      data-card
+      {...props}
+    >
+      <UI as="div" data-card-content>
+        {title && <Title>{title}</Title>}
+        {children}
+        {footer && (
+          <UI as="div" data-card-footer>
+            {footer}
+          </UI>
+        )}
+      </UI>
+    </UI>
+  )
+}
 
 export default Card
 Card.displayName = 'Card'
--- libs/react/fpkit/src/components/cards/flex-card.tsx
+++ libs/react/fpkit/src/components/cards/flex-card.tsx
@@ -1,7 +1,15 @@
 import Card from './card'
 
 const FlexCard = () => {
-  return
+  return (
+    <Card>
+      <Card.Title />
+      <Card.Content>
+        <div>Flexible Content</div>
+      </Card.Content>
+      <div style={{ padding: '1rem' }}>Footer</div>
+    </Card>
+  )
 }
 
 export default FlexCard
